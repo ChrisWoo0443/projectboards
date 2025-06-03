86c86d3c3a6c87ab4d32cd1794866afca817ccee
ChrisWoo0443 <chefwoodini@gmail.com>
2025-06-02 17:54:12 -0700

    changed directory

---
+++ src/pages/Index.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+// import { KanbanBoard } from "../components/ui/KanbanBoard";
+// import { CalendarView } from "../components/CalendarView";
+// import { ListView } from "../components/ui/ListView";
+// import { Header } from "../components/Header";
+// import { CreateTaskModal } from "../components/CreateTaskModal";
+// import { ColumnManager } from "../components/ColumnManager";
+// import { Task, Column } from "../types/kanban";
+// import { Tabs, TabsContent, TabsList, TabsTrigger } from "../components/ui/tabs";
+
+const Index = () => {
+
+};
+
+export default Index;
